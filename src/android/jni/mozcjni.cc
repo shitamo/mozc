255a2d6b57a3607e0e076972ba267701bc23b721
Mozc team <noreply@google.com>
2026-09-08 17:19:14 +0000

    Renames MozcJNI to MozcJni per Google Java Style §5.3. Updates the JNI exported C++ native function in mozcjni.cc, the Proguard keep rule in base.pgcfg, session BUILD target sources, and Java call sites.
    PiperOrigin-RevId: 977982563

--- src/android/jni/mozcjni.cc
+++ src/android/jni/mozcjni.cc
@@ -182,7 +182,7 @@ jstring JNICALL getDataVersion(JNIEnv* env) {
 extern "C" {
 
 JNIEXPORT jboolean JNICALL
-Java_com_google_android_apps_inputmethod_libs_mozc_session_MozcJNI_initialize(
+Java_com_google_android_apps_inputmethod_libs_mozc_session_MozcJni_initialize(
     JNIEnv* env, jclass clazz) {
   if (!env) {
     // Fatal error. No way to recover.
